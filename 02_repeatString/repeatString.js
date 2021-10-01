9865f908207578975777d116d751ba5eb10b3534
Jent-007 <jeansijent09@gmail.com>
2021-10-01 12:58:55 +0800

    Edited repeatString.js and repeatString.spec.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    for (let i = 0; i < 1000; i++) {
+        str.repeat(i);
+    }
+    if (num < 0) {
+        return 'ERROR'
+    } else if (str == '') {
+        return str;
+    } else {
+        return str.repeat(num);
+    }
 };
 
 // Do not edit below this line
